783eaaf0ca436f1598aa872d10bd8542c6617eb5
Israel Sam <ace@bizinkonlie.com>
2022-10-24 17:40:19 +0800

    Change folder names.

--- IPDataWebAPI/Models/WhoIsData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IPDataWebAPI.Models
-{
-    public class WhoIsData
-    {
-        public string CountryCode { get; set; }
-
-        public WhoIsCurrency Currency { get; set; }
-    }
-}
